5d78cc140d90b34bfb44ebb516096293a7158697
Gaming4JC <g4jc@bulletmail.org>
2018-08-21 17:36:45 -0400

    backport comm-central Bug 1079280 - Don't transmit email when imap append response is bad.

--- mailnews/imap/src/nsImapProtocol.cpp
+++ mailnews/imap/src/nsImapProtocol.cpp
@@ -6134,7 +6134,11 @@ void nsImapProtocol::UploadMessageFromFile (nsIFile* file,
     if (NS_FAILED(rv)) goto done;
 
     if (!useLiteralPlus)
-      ParseIMAPandCheckForNewMail();
+    {
+       ParseIMAPandCheckForNewMail();
+      if (!GetServerStateParser().LastCommandSuccessful())
+        goto done;
+    }
 
     totalSize = fileSize;
     readCount = 0;
