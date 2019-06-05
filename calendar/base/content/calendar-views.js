2a0e207339dbafebe13d382ce20b71f0536242bd
Gaming4JC <g4jc@hyperbola.info>
2019-06-05 08:45:45 -0400

    backport c-c 1553000 - Get last-used calendar view directly from the XUL store.

--- calendar/base/content/calendar-views.js
+++ calendar/base/content/calendar-views.js
@@ -601,9 +601,10 @@ function goToDate(aDate) {
  * @return          The last calendar view.
  */
 function getLastCalendarView() {
-    let deck = getViewDeck();
-    if (deck.selectedIndex > -1) {
-        let viewNode = deck.childNodes[deck.selectedIndex];
+    if (Services.xulStore.hasValue(document.location.href, "view-deck", "selectedIndex")) {
+        let deck = getViewDeck();
+        let selectedIndex = Services.xulStore.getValue(document.location.href, "view-deck", "selectedIndex");
+        let viewNode = deck.childNodes[selectedIndex];
         return viewNode.id.replace(/-view/, "");
     }
 
