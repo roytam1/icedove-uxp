e6295feec517ed5dd1f690b9123a5e07c7f3cb6a
Gaming4JC <g4jc@hyperbola.info>
2019-06-14 19:25:53 -0400

    Bump UA to reduce fingerprinting

--- mail/branding/icedove/icedove-uxp-branding.js
+++ mail/branding/icedove/icedove-uxp-branding.js
@@ -17,4 +17,4 @@ pref("mailnews.start_page.override_url", "about:");
 pref("app.vendorURL", "https://wiki.hyperbola.info/doku.php?id=en:project:icedove-uxp");
 
 // We appear as Thunderbird to avoid fingerprinting risks
-pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:60.0) Gecko/20100101 Thunderbird/60.7.0");
+pref("general.useragent.override", "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:60.0) Gecko/20100101 Thunderbird/60.7.1");
