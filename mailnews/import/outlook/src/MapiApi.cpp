fa0f28a17065cb573ce45ac542a09e4453623f62
Gaming4JC <g4jc@hyperbola.info>
2019-10-03 21:40:47 -0400

    backport c-c 1584473 - Fix undefined HRESULT codes in class CMapiApi in MapiApi.cpp.

--- mailnews/import/outlook/src/MapiApi.cpp
+++ mailnews/import/outlook/src/MapiApi.cpp
@@ -887,7 +887,7 @@ BOOL CMapiApi::IterateStores(CMapiFolderList& stores)
 
   if (!m_lpSession) {
     MAPI_TRACE0("IterateStores called before session is open\n");
-    m_lastError = -1;
+    m_lastError = E_UNEXPECTED;
     return FALSE;
   }
 
@@ -1124,7 +1124,7 @@ CMsgStore *  CMapiApi::FindMessageStore(ULONG cbEid, LPENTRYID lpEid)
 {
   if (!m_lpSession) {
     MAPI_TRACE0("FindMessageStore called before session is open\n");
-    m_lastError = -1;
+    m_lastError = E_UNEXPECTED;
     return NULL;
   }
 
@@ -1843,7 +1843,7 @@ BOOL CMapiFolderContents::SetUpIter(void)
   }
 
   if (ulObjType != MAPI_FOLDER) {
-    m_lastError = -1;
+    m_lastError = E_UNEXPECTED;
     MAPI_TRACE0("CMapiFolderContents - bad object type, not a folder.\n");
     return FALSE;
   }
